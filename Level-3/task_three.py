b093f7facba89f5aaa6b5068d2c0fcc0c3806a02
Kacper <kacperlikus@hotmail.com>
2026-02-27 15:19:40 +0000

    Just realised what it was asking me to do... need to change all the prices to their respective values....

--- Level-3/task_three.py
+++ Level-3/task_three.py
@@ -9,7 +9,9 @@ def get_both_VAT(total_price_string: str) -> tuple[str, str]:
         total_price = float(total_price_string)
         vat_add_on = total_price * 0.20
         vat_included = total_price + vat_add_on
-        return str(vat_add_on), str(vat_included)
+        vat = f"VAT: £{vat_add_on:.2f}\n"
+        total_inc_vat = f"Total inc VAT: £{vat_included:.2f}"
+        return str(vat), str(total_inc_vat)
     except:
         raise "WRONG VALUE TYPE INPUT TO VAT CALCULATOR"
 
@@ -20,26 +22,22 @@ def generate_invoice(receipt_string: str) -> str:
         return "Receipt was empty"
     # Read line by line
     receipt_lines = receipt_string.split("\n")
-    invoice_string = ""
-    VAT_price = 0
+    invoice_string = "VAT RECEIPT\n\n"
     for line in receipt_lines:
-        print(line, "and", line[:6])
+        # print(line, "and", line[:6])
         # If first word of line is Total:
         if line[:6] == "Total:":
             #   Search the line for the total price: i.e. everything after £ on that line
             total_price = line[-4:]
-            print(total_price)
+            # print(total_price)
             #   Add new line to make a gap before total line displayed
             invoice_string += "\n" + line + "\n"
             # Convert the price to float, calculate VAT
             # Add -added- VAT and Total inc VAT as two lines on the end.
             added_VAT, total_with_VAT = get_both_VAT(total_price)
-            vat = f"VAT: £{added_VAT}\n"
-            total_inc_vat = f"Total inc VAT: £{total_with_VAT}\n"
-            invoice_string += vat + total_inc_vat
+            invoice_string += added_VAT + total_with_VAT
         else:
             invoice_string += line + "\n"
-    print("\nInvoice string!!!:")
     return invoice_string  # return the invoice string
 
 
@@ -49,4 +47,3 @@ Milk x 1 - £0.80
 Butter x 1 - £1.20
 Total: £5.60"""
     print(generate_invoice(receipt_string))
-    print(generate_invoice(""))
