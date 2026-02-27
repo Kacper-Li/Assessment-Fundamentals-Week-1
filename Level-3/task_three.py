8c35c6cbb1d32060f1fd02ab0c11882ce9f2b589
Kacper <kacperlikus@hotmail.com>
2026-02-27 14:59:47 +0000

    Correct sliced the string to get the total line, and the price from the back of it.

--- Level-3/task_three.py
+++ Level-3/task_three.py
@@ -8,12 +8,24 @@ def generate_invoice(receipt_string: str) -> str:
     if not receipt_string:
         return "Receipt was empty"
     # Read line by line
-    # If first word of line is Total:
-    #   Add new line to make a gap before total line displayed
-    #   Search the line for the total price: i.e. everything after £ on that line
-    # Convert the price to float, calculate VAT
-    # Add -added- VAT and Total inc VAT as two lines on the end.
-    return "Not done yet :P"  # return the invoice string
+    receipt_lines = receipt_string.split("\n")
+    invoice_string = ""
+    VAT_price = 0
+    for line in receipt_lines:
+        print(line, "and", line[:6])
+        # If first word of line is Total:
+        if line[:6] == "Total:":
+            total_price = line[-4:]
+            print(total_price)
+            invoice_string += "\n" + line + "\n"
+            #   Add new line to make a gap before total line displayed
+            #   Search the line for the total price: i.e. everything after £ on that line
+            # Convert the price to float, calculate VAT
+            # Add -added- VAT and Total inc VAT as two lines on the end.
+        else:
+            invoice_string += line + "\n"
+    print("\nInvoice string!!!:")
+    return invoice_string  # return the invoice string
 
 
 if __name__ == "__main__":
