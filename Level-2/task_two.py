d00ad3bc74a1bbb1af961271e12145bde13cca21
Kacper <kacperlikus@hotmail.com>
2026-02-27 14:25:45 +0000

    Altered add to basket function to only accept unique items via name/price. Began to alter the receipt to account for duplicates. Setup new global dict item_count that records name of an item and the amoutn of times counted

--- Level-2/task_two.py
+++ Level-2/task_two.py
@@ -4,6 +4,10 @@ The only variable you are allowed to use in the global scope is the basket below
 """
 
 basket = []
+item_count = {
+    # "name": "name",
+    # "count": 0
+}
 
 #####
 #
@@ -13,19 +17,39 @@ basket = []
 
 
 def add_to_basket(item: dict) -> list:
-    pass
-
+    """Return basket list with item appended."""
+    if item in basket:
+        item_count[item['name']] += 1
+    else:
+        item_count[item['name']] = 1
+        basket.append(item)
+    return basket
 
 
 def generate_receipt(basket: list) -> str:
-    pass
+    """Returns a built string of each item in the basket."""
+    if basket != []:
+        total = 0.00
+        receipt = ""
+        for item in basket:
+            number_of_item = item_count[item['name']]
+            price_of_item = item['price']
+            collective_price = price_of_item * number_of_item
+            total += collective_price
+            if item['price'] == 0.00:
+                receipt += f"{item['name']} - Free\n"
+            else:
+                receipt += f"{item['name']} - £{collective_price:.2f}\n"
+        receipt += f"Total: £{total:.2f}"
+        return receipt
+    return "Basket is empty"  # return the receipt string
+
 
 #####
 #
 # COPY YOUR CODE FROM LEVEL 1 ABOVE
 #
 #####
-
 
 if __name__ == "__main__":
     add_to_basket({
@@ -35,6 +59,10 @@ if __name__ == "__main__":
     add_to_basket({
         "name": "Bread",
         "price": 1.80
+    })
+    add_to_basket({
+        "name": "Bread",
+        "price": 1.70
     })
     add_to_basket({
         "name": "Milk",
